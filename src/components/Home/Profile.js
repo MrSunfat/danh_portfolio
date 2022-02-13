a60330e22d03ecbe4062d340c24cedebda9a38e2
MrSunfat <trannhatdanh15062001@gmail.com>
2022-02-13 22:35:30 +0700

    add gradient bg, gradient text and typewriter effect

--- src/components/Home/Profile.js
+++ src/components/Home/Profile.js
@@ -1,16 +1,16 @@
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row } from 'reactstrap';
 
 function Profile({ children, img, role }) {
     return (
         <div className="profile-container padding40">
             <Container fluid="md" className="profile">
                 <Row className="profile__row">
-                    <div className="profile__details">
+                    <div className="profile__details typewritter">
                         <h2 className="profile__details__hello">
                             Xin chào, tên tôi là
                         </h2>
                         <p className="profile__details__fullname">{children}</p>
-                        <p className="profile__details__role">{`${role}`}</p>
+                        <h2 className="profile__details__role">{`${role}`}</h2>
                     </div>
                     <div className="profile__picture">
                         <img src={img} alt={children} />
