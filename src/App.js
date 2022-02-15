63360b7091b42dd938de9466bd84a928b0007a27
MrSunfat <trannhatdanh15062001@gmail.com>
2022-02-15 16:25:19 +0700

    opacity header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import { Home, About, Projects, Contact, Footer, Bar } from './components';
-import { BsList } from 'react-icons/bs';
-import { useGlobalContext } from './context';
 
 function App() {
-    const { isBarOpen, toggleBar } = useGlobalContext();
-
     return (
         <div className="App">
-            <div className={`bar on-bar ${!isBarOpen && 'notActive'}`}>
-                <BsList
-                    className="bar__icon"
-                    onClick={() => toggleBar(!isBarOpen)}
-                />
-                {isBarOpen && <Bar />}
-            </div>
+            <Bar />
             <div className="portfolio-container">
                 <Home />
                 <About />
--- src/components/Bar/index.js
+++ src/components/Bar/index.js
@@ -1,35 +1,46 @@
-import React from 'react';
+import { useGlobalContext } from '../../context';
 import {
     BsHouseDoor,
     BsFileEarmarkText,
     BsChatLeftDots,
     BsJournalBookmarkFill,
+    BsList,
 } from 'react-icons/bs';
 
 function Bar() {
+    const { isBarOpen, toggleBar } = useGlobalContext();
+
     return (
-        <ul className="bar__list">
-            <li className="bar__item">
-                <a href="#home" className="bar__item__link">
-                    <BsHouseDoor className="icon" />
-                </a>
-            </li>
-            <li className="bar__item">
-                <a href="#about" className="bar__item__link">
-                    <BsFileEarmarkText className="icon" />
-                </a>
-            </li>
-            <li className="bar__item">
-                <a href="#projects" className="bar__item__link">
-                    <BsJournalBookmarkFill className="icon" />
-                </a>
-            </li>
-            <li className="bar__item">
-                <a href="#contact" className="bar__item__link">
-                    <BsChatLeftDots className="icon" />
-                </a>
-            </li>
-        </ul>
+        <div className={`bar on-bar ${!isBarOpen && 'notActive'}`}>
+            <BsList
+                className="bar__icon"
+                onClick={() => toggleBar(!isBarOpen)}
+            />
+            {isBarOpen && (
+                <ul className="bar__list">
+                    <li className="bar__item">
+                        <a href="#home" className="bar__item__link">
+                            <BsHouseDoor className="icon" />
+                        </a>
+                    </li>
+                    <li className="bar__item">
+                        <a href="#about" className="bar__item__link">
+                            <BsFileEarmarkText className="icon" />
+                        </a>
+                    </li>
+                    <li className="bar__item">
+                        <a href="#projects" className="bar__item__link">
+                            <BsJournalBookmarkFill className="icon" />
+                        </a>
+                    </li>
+                    <li className="bar__item">
+                        <a href="#contact" className="bar__item__link">
+                            <BsChatLeftDots className="icon" />
+                        </a>
+                    </li>
+                </ul>
+            )}
+        </div>
     );
 }
 
